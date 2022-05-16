f233ff33ea36f06a2bfc64864c72b14ddbeaa513
Sohel Mia <sohelcse1999@gmail.com>
2022-05-16 21:42:19 +0600

    update

--- var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2022-05-16 15:23:55
-         compiled from layout/content-footer.html */ ?>
-<?php if ($this->_tpl_vars['showContentFrame']): ?>
-</div>
-<?php endif; ?>
