2a18142f86ca837f7acdf4ff766579b76ecf5b1b
Sohel Mia <sohelcse1999@gmail.com>
2022-05-16 20:33:53 +0600

    first commit

---
+++ var/templates_compiled/%%82^82C^82C6DE93%%main.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2022-05-16 14:30:14
+         compiled from dashboard/main.html */ ?>
+
+<iframe src="<?php echo $this->_tpl_vars['dashboardURL']; ?>
+" width="100%" height="850" frameborder="0" marginwidth="0" marginheight="0"></iframe>
---
+++ var/templates_compiled/%%EC^ECD^ECDFE081%%passthrough.html.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.18, created on 2022-05-16 14:30:15
+         compiled from passthrough.html */ ?>
+<?php echo $this->_tpl_vars['content']; ?>
